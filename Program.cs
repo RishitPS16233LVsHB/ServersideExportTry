2956306bddca2e776d35936bf18bca72cddaf353
RishitPS16233LVsHB <108234609+RishitPS16233LVsHB@users.noreply.github.com>
2023-11-08 11:23:27 +0530

    added to PDF generation method using DataTable
    using the datatable method for pdf generation there will be no styling not like html content one you may be able to style the content

--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using OfficeOpenXml;
 using System.Data;
+using System.Data.SqlClient;
 using System.IO;
 using iTextSharp.text;
 using iTextSharp.text.pdf;
 using iTextSharp.tool.xml;
-
+using System.Data.Common;
 
 namespace ServerSideExportTry
 {
@@ -28,26 +29,76 @@ namespace ServerSideExportTry
             dataTable.Rows.Add(new object[] { 3, "raj", 21, 20000});
             dataTable.Rows.Add(new object[] { 3, "muneer", 21, 25000});
 
+            //string query = "select * from Sales.order_items"
+            using (SqlConnection connection = new SqlConnection("Data Source=DESKTOP-T8Q9USL;Initial Catalog=BikeStoreDatabase;Integrated Security=True"))
+            {
+                dataTable.Rows.Clear();
+                dataTable.Columns.Clear();
+                dataTable.Clear();
+                SqlDataAdapter adp = new SqlDataAdapter("select * from Sales.order_items", connection);
+                adp.Fill(dataTable);                
+            }
+
+            // this is license based but we can use NonCommercial one to solve our problem
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
             using (ExcelPackage pack = new ExcelPackage())
             { 
                 ExcelWorksheet worksheet = pack.Workbook.Worksheets.Add("employee details");
-                worksheet.Cells["A1"].LoadFromDataTable(dataTable);
+                System.Collections.Generic.List<string> columnName = new System.Collections.Generic.List<string>();
+                foreach (DataColumn dc in dataTable.Columns)
+                    columnName.Add(dc.ColumnName);
+
+                worksheet.Cells["A1"].LoadFromCollection<string>(columnName);
+                worksheet.Cells["A2"].LoadFromDataTable(dataTable);
 
                 FileInfo fileInfo = new FileInfo("Employee.xlsx");
                 pack.SaveAs(fileInfo);
-
-
-
             }
+
 
             string htmlContent = "<html><head><title>Sample HTML to PDF</title></head><body><h1>Hello, World!</h1><img src=\"D:\\ServerSideExportTry\\ServerSideExportTry\\bin\\Debug\\net5.0\\Image\\Geralt.jpg\"/></body></html>";
             string outputPath = "output.pdf";
 
-            GeneratePdfFromHtml(htmlContent, outputPath);
+            GeneratePdfFromDataTable(dataTable, outputPath);
             Console.WriteLine("Excel and PDF generated successfully.");
             Console.Read();
         }
+        public static void GeneratePdfFromDataTable(DataTable dataTable, string outputPath)
+        {
+            using (FileStream fs = new FileStream(outputPath, FileMode.Create))
+            {
+                using (Document doc = new Document())
+                {
+                    PdfWriter writer = PdfWriter.GetInstance(doc, fs);
+                    doc.Open();
 
+                    // Create a PDF table
+                    PdfPTable pdfTable = new PdfPTable(dataTable.Columns.Count);
+
+                    // Add table headers
+                    foreach (DataColumn column in dataTable.Columns)
+                    {
+                        PdfPCell cell = new PdfPCell(new Phrase(column.ColumnName));
+                        pdfTable.AddCell(cell);
+                    }
+
+                    // Add table data
+                    foreach (DataRow row in dataTable.Rows)
+                    {
+                        foreach (object item in row.ItemArray)
+                        {
+                            PdfPCell cell = new PdfPCell(new Phrase(item.ToString()));
+                            pdfTable.AddCell(cell);
+                        }
+                    }
+
+                    // Add the table to the document
+                    doc.Add(pdfTable);
+
+                    doc.Close();
+                }
+            }
+        }
         public static void GeneratePdfFromHtml(string htmlContent, string outputPath)
         {
             using (FileStream fs = new FileStream(outputPath, FileMode.Create))
